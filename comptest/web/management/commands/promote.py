df87125dd3ee33faae386832cdfa7cba1a4cc495
YuviPanda <yuvipanda@gmail.com>
2024-07-25 22:35:08 -0700

    Add command to promote user to superuser

---
+++ comptest/web/management/commands/promote.py
@@ -0,0 +1,15 @@
+from argparse import ArgumentParser
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    def add_arguments(self, parser: ArgumentParser):
+        parser.add_argument('username', help='Name of existing user to promote as superuser')
+
+    def handle(self, *args, **options):
+        username = options['username']
+        user = User.objects.filter(username=username).get()
+        user.is_staff = True
+        user.is_admin = True
+        user.is_superuser = True
+        user.save()
