95386124e96a993352f5f3c16d38f98767260e33
YuviPanda <yuvipanda@gmail.com>
2024-07-26 18:31:05 -0700

    Remove unused old debug code

--- comptest/web/apps.py
+++ comptest/web/apps.py
@@ -2,14 +2,6 @@ from allauth.socialaccount.signals import pre_social_login
 from django.apps import AppConfig
 
 
-def login_signal(request, sociallogin, **kwargs):
-    print(request)
-    print(sociallogin)
-
-
 class WebConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "web"
-
-    def ready(self):
-        pre_social_login.connect(login_signal)
