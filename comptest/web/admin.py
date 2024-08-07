d8e52c5aef4a384351fc6fe937177202576b4afd
Georgiana Dolocan <georgiana.dolocan@gmail.com>
2024-08-07 11:24:54 +0300

    Register the project model in the admin panel

--- comptest/web/admin.py
+++ comptest/web/admin.py
@@ -2,7 +2,7 @@ from allauth.account.decorators import secure_admin_login
 from django.contrib import admin
 from reversion.admin import VersionAdmin
 
-from .models import Evaluation, Page, Submission
+from .models import Evaluation, Page, Project, Submission
 
 
 @admin.register(Page)
@@ -10,6 +10,6 @@ class PageAdmin(VersionAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 
-admin.site.register([Submission, Evaluation])
+admin.site.register([Submission, Evaluation, Project])
 
 admin.site.login = secure_admin_login(admin.site.login)
