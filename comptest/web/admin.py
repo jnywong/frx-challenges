22524c06bb0f1b3c1bbcfff2c30edeb4e618839f
jnywong <jnywong.pro@gmail.com>
2024-10-11 11:40:47 +0100

    Register Submission and Version models

--- comptest/web/admin.py
+++ comptest/web/admin.py
@@ -2,7 +2,7 @@ from allauth.account.decorators import secure_admin_login
 from django.contrib import admin
 from reversion.admin import VersionAdmin
 
-from .models import ContentFile, Evaluation, Page, Project, Version
+from .models import ContentFile, Evaluation, Page, Submission, Version
 
 
 @admin.register(Page)
@@ -15,6 +15,6 @@ class ContentFileAdmin(VersionAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 
-admin.site.register([Version, Evaluation, Project])
+admin.site.register([Version, Evaluation, Submission])
 
 admin.site.login = secure_admin_login(admin.site.login)
