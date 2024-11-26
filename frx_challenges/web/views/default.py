6a57d1dad74052fc064273a16ecad1f88159fc2d
jnywong <jnywong.pro@gmail.com>
2024-11-26 12:23:49 +0000

    ✨ Add configurable metadata display in leaderboard

--- frx_challenges/web/views/default.py
+++ frx_challenges/web/views/default.py
@@ -22,11 +22,25 @@ def leaderboard(request: HttpRequest) -> HttpResponse:
     results = []
     all_submissions = Submission.objects.all()
     for sub in all_submissions:
+        # Get best scoring version
         bv = sub.best_version
         if not bv:
             # Only display submissions with at least one 'best version'
             continue
-        results.append({"submission": sub, "best_version": bv})
+        # Get submission metadata
+        metadata = []
+        for k, v in settings.SITE_SUBMISSION_FORM_SCHEMA["properties"].items():
+            if v.get("leaderboard_display"):
+                metadata.append(
+                    sub.metadata.get(k),
+                )
+        results.append(
+            {
+                "submission": sub,
+                "best_version": bv,
+                "metadata": metadata,
+            }
+        )
 
     def sort_key_func(r):
         bv: Version = r["best_version"]
@@ -44,6 +58,18 @@ def leaderboard(request: HttpRequest) -> HttpResponse:
         return sort_key
 
     results = sorted(results, key=sort_key_func)
+
+    metadata_display = []
+    for k, v in settings.SITE_SUBMISSION_FORM_SCHEMA["properties"].items():
+        if v.get("leaderboard_display"):
+            metadata_display.append(v.get("title"))
+
     return render(
-        request, "results.html", {"results": results, "description": description}
+        request,
+        "results.html",
+        {
+            "results": results,
+            "description": description,
+            "metadata_display": metadata_display,
+        },
     )
